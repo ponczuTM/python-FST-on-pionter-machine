5ee5e5910df2db12d408e95d90ece3b0a71383cc
ponczuTM <mroczkowskioliwer10@gmail.com>
2023-05-15 16:07:50 +0200

    tree_to_array

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import mroczkowski_library.functions as mo
-
+import os
+os.system("cls")
 """
 mo.bsort(array, "test")
 print()
 mo.bsort(array, "show")
 """
+
 root = None
 root = mo.insert(root, 50)
 root = mo.insert(root, 25)
@@ -26,12 +28,19 @@ root = mo.insert(root, 7)
 
 array = [5, 2, 35656546, 4, 1, 3, 3, 12, 123, 135, 546, 456]
 
-mo.display_tree(root)
+#mo.display_tree(root)
 #print()
 
-sorted_arr = mo.bsort(array)
+#sorted_arr = mo.bsort(array)
 #print(sorted_arr, "\n")
 
-mo.insertion_sort_pointer(array)
+#mo.insertion_sort_pointer(array)
 #print(array, "\n")
-mo.FST(root)
+#mo.FST(root)
+#print(mo.save_binary_tree_to_array(root))
+
+array_tree = mo.save_binary_tree_to_array(root)
+print(array_tree)
+print(array_tree[0])
+print(array_tree[3])
+print(array_tree[24])
--- mroczkowski_library/functions.py
+++ mroczkowski_library/functions.py
@@ -43,38 +43,37 @@ def test(array, *args):
         print(f"Przekazałeś do funkcji tablicę liczb oraz cztery napisy: {args[0]}, {args[1]}, {args[2]} i {args[3]}")
 
 
-######################################################
-
-class PointerMachine:
+class pointerMachine:
     def __init__(self, array):
         self.array = array
         self.index = 0
         self.value = None
         self.counter = 0
         self.registers = [None] * 4
-    
     def get(self):
         self.value = self.array[self.index]
         self.counter += 1
-    
     def set(self, value):
         self.array[self.index] = value
         self.counter += 1
-    
     def go_R(self):
         self.index = self.index + 1
         self.counter += 1
-    
     def go_L(self):
         self.index = self.index - 1
         self.counter += 1
-    
-    def is_end(self):
-        return self.index >= len(self.array)
+    def go_U(self):
+        self.index = self.index - len(self.registers)
+        self.counter += 1
+    def go_D(self):
+        self.index = self.index + len(self.registers)
+        self.counter += 1
+    #def is_end(self):
+        #return self.index >= len(self.array)
 
-
-def insertion_sort_pointer(array):
-    machine = PointerMachine(array)
+"""
+def insertion_sort_last_dir(array):
+    machine = last_dirMachine(array)
     for i in range(1, len(array)):
         machine.index = i
         machine.get()
@@ -82,7 +81,7 @@ def insertion_sort_pointer(array):
             machine.array[machine.index] = machine.array[machine.index-1]
             machine.go_L()
         machine.array[machine.index] = machine.value
-
+"""
 
 ###############################
 
@@ -92,7 +91,7 @@ class Node:
         self.right = None
         self.val = key
  
- 
+
 def insert(root, key):
     if root is None:
         return Node(key)
@@ -109,50 +108,98 @@ def display_tree(node, level=0, label='>'):
         print(' ' * 4 * level + label + str(node.val))
         display_tree(node.left, level + 1, 'L')
 
-def search_in_tree(root, search_number, pointer):
-    current = root
+def search_in_tree(root, last_dir, search_number, current):
+    if current is None:
+        current = root
+        last_dir = 1 if current.val > search_number else -1
     parent = None
     steps = 0
-
     while current is not None:
         if current.val == search_number:
-            return (current, steps)
+            return (current, last_dir, steps)
         elif current.val < search_number:
             parent = current
             current = current.right
             steps += 1
+            #print("right: " + str(current.val))
         else:
             parent = current
             current = current.left
             steps += 1
+            print("left: " + str(current.val))
 
         if parent is not None:
-            if parent.val < search_number and pointer != 1:
-                pointer = 1
+            if parent.val < search_number and last_dir != 1:
+                last_dir = 1
                 current = parent
                 parent = None
                 steps += 1
-            elif parent.val > search_number and pointer != -1:
-                pointer = -1
+                print("up 1: " + str(current.val))
+            elif parent.val > search_number and last_dir != -1:
+                last_dir = -1
                 current = parent
                 parent = None
                 steps += 1
+                print("up -1: " + str(current.val))
+    return (None, last_dir, steps)
 
-    return (None, steps)
+"""
+            R96   <-   S
+        R89
+            L78
+    R75
+            R73
+        L68
+            L53
+>50
+            R47
+        R37       <-   P
+            L29
+    L25
+            R22
+        L16
+            L7
+"""
+
+def save_binary_tree_to_array(root):
+    def preorder_traversal(node):
+        if node is None:
+            return -1  # Return -1 for non-existent node
+        index = len(array) // 3
+        # Temporarily store None for left and right indices. We'll fill these in after the recursive calls.
+        array.extend([node.val, None, None])
+        left_index = preorder_traversal(node.left)
+        right_index = preorder_traversal(node.right)
+        # Calculate steps from current node to its children
+        if left_index != -1:
+            array[index * 3 + 1] = (left_index - index) * 3 - 1
+        if right_index != -1:
+            array[index * 3 + 2] = (right_index - index) * 3 - 2
+        return index
+    array = []
+    preorder_traversal(root)  # Start pre-order traversal
+    return array
 
 
 def FST(root, show=None): 
     if show:
         print("nice try but there is no code to print :)")
     elif show is not None:
-        print("Invalid argument. Use 'show' to display sorting function.")
+        print("Invalid argument. Use 'show' to display function.")
     else:
-        search_number = int(input("enter the number you are looking for: "))
-        pointer = int(input("enter where you want the pointer: "))
+        #search_number = int(input("enter the number you are looking for: "))
+        #pointer = int(input("enter where you want the pointer: "))
+        print("--------------------")
+        search_number = 96
+        pointer = 7
+        last_dir = 0
+        result, last_dir, steps = search_in_tree(root, last_dir, pointer, None)
+        print("--------------------")
+        result, last_dir, steps = search_in_tree(root, last_dir, search_number, result)
 
-        result, steps = search_in_tree(root, search_number, pointer)
+        
         if result is not None:
-            print(f"Number {search_number} found in binary tree") # wwierzchołku o wartości {result.val}")
+            print(f"Number {search_number} found in binary tree") # w wierzchołku o wartości {result.val}")
         else:
-            print(f"Number {search_number} NOT fount in binary tree")
+            print(f"Number {search_number} NOT found in binary tree")
         print(f"Number of steps: {steps}")
